ee6b15635ef616ce94acb67e56316e4e4cb6fb7d
BlackD9 <phongvn215@gmail.com>
2025-11-24 10:18:37 -0700

    fixing typo game-fetch

--- src/public_html/js/game-fetch.js
+++ src/public_html/js/game-fetch.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     container.innerHTML = '<p id="loading">Loading news...</p>';
 
     const queries = [
-        '"gameing"',
+        '"gaming"',
         '"video game"',
         '"esports"',
         '"pc gaming"',
