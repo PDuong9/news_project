a83054b68c573dc62f26a01cd6787ba68deb01e7
BlackD9 <phongvn215@gmail.com>
2025-11-24 10:10:18 -0700

    fixing backend

--- src/routes/news.js
+++ src/routes/news.js
@@ -20,9 +20,9 @@ router.get('/source/:source', async (req, res) => {
 
 // Top-headlines by query
 router.get('/query/:query', async (req, res) => {
-    const queries = req.params.query;
+    const query = req.params.query;
     const apiKey = process.env.NEWS_API_KEY;
-    const url = `https://newsapi.org/v2/everything?q=${encodeURIComponent(queries)}&language=en&sortBy=popularity&apiKey=${apiKey}`;
+    const url = `https://newsapi.org/v2/everything?q="${encodeURIComponent(query)}"&language=en&sortBy=relevancy&apiKey=${apiKey}`;
     fetchAndRespond(url, res);
 });
 
